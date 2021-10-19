c314a1f8865b5fb078e1e4670e9dcd9b77993438
Wei-Yung <weiyunghsu@gmail.com>
2021-10-19 10:43:28 +0800

    a basic map can show on MapFragment.kt

--- app/src/main/java/com/weiyung/intotheforest/map/MapFragment.kt
+++ app/src/main/java/com/weiyung/intotheforest/map/MapFragment.kt
@@ -1,16 +1,26 @@
 package com.weiyung.intotheforest.map
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.weiyung.intotheforest.addarticle.AddArticleViewModel
-import com.weiyung.intotheforest.databinding.FragmentAddarticleBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.weiyung.intotheforest.R
 import com.weiyung.intotheforest.databinding.FragmentMapBinding
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
     private lateinit var binding: FragmentMapBinding
     private lateinit var viewModel: MapViewModel
     override fun onCreateView(
@@ -21,9 +31,27 @@ class MapFragment : Fragment() {
         viewModel =
             ViewModelProvider(this).get(MapViewModel::class.java)
         binding = FragmentMapBinding.inflate(inflater, container, false)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
         val root: View = binding.root
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
         return binding.root
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+//        mMap.setMyLocationEnabled(true); // 右上角的定位功能；這行會出現紅色底線，不過仍可正常編譯執行
+        mMap.getUiSettings().setZoomControlsEnabled(true);  // 右下角的放大縮小功能
+        mMap.getUiSettings().setCompassEnabled(true);       // 左上角的指南針，要兩指旋轉才會出現
+        mMap.getUiSettings().setMapToolbarEnabled(true);    // 右下角的導覽及開啟 Google Map功能
+        // Add a marker in Sydney and move the camera
+        val appWorksSchool = LatLng(25.04247739624179, 121.56487872944777)
+        mMap.addMarker(MarkerOptions().position(appWorksSchool).title("AppWorksSchool Stylish Store \n台北市基隆路一段178號"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(appWorksSchool))
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(18F))
+    }
 }
+
