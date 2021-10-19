4f9b7143d658f1d6ed3ef92ca8a86c83a9d1fee1
Wei-Yung <weiyunghsu@gmail.com>
2021-10-19 14:08:36 +0800

    add MyLocation function

--- app/src/main/java/com/weiyung/intotheforest/map/MapFragment.kt
+++ app/src/main/java/com/weiyung/intotheforest/map/MapFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -43,15 +44,24 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-//        mMap.setMyLocationEnabled(true); // 右上角的定位功能；這行會出現紅色底線，不過仍可正常編譯執行
-        mMap.getUiSettings().setZoomControlsEnabled(true);  // 右下角的放大縮小功能
-        mMap.getUiSettings().setCompassEnabled(true);       // 左上角的指南針，要兩指旋轉才會出現
-        mMap.getUiSettings().setMapToolbarEnabled(true);    // 右下角的導覽及開啟 Google Map功能
+        fun setupPermission() {
+            if (ContextCompat.checkSelfPermission(this.binding.root.context, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 999)
+            } else {
+                mMap.isMyLocationEnabled = true
+            }
+        }
+        setupPermission()
+//        mMap.isMyLocationEnabled = true; // 右上角的定位功能
+        mMap.uiSettings.isZoomControlsEnabled = true;  // 右下角的放大縮小功能
+        mMap.uiSettings.isCompassEnabled = true;       // 左上角的指南針，要兩指旋轉才會出現
+        mMap.uiSettings.isMapToolbarEnabled = true;    // 右下角的導覽及開啟 Google Map功能
         // Add a marker in Sydney and move the camera
-        val appWorksSchool = LatLng(25.04247739624179, 121.56487872944777)
-        mMap.addMarker(MarkerOptions().position(appWorksSchool).title("AppWorksSchool Stylish Store \n台北市基隆路一段178號"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(appWorksSchool))
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(18F))
+        val appWorksSchoolPeak = LatLng(25.042477, 121.564879)
+        mMap.addMarker(MarkerOptions().position(appWorksSchoolPeak).title("AppWorksSchool Peak \n台北市基隆路一段178號"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(appWorksSchoolPeak,18F))
+//        mMap.animateCamera(CameraUpdateFactory.zoomTo(18F))
     }
 }
 
